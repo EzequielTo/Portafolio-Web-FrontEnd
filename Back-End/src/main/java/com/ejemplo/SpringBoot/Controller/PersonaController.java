313a3c8addef7025db809d2463e7ac3622159468
Ezequiel <122380414+EzequielTo@users.noreply.github.com>
2023-05-10 20:46:51 -0300

    agregado interaccion entre front y back ends Y routeo basico

--- Back-End/src/main/java/com/ejemplo/SpringBoot/Controller/PersonaController.java
+++ Back-End/src/main/java/com/ejemplo/SpringBoot/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.ejemplo.SpringBoot.Entity.Persona;
 import com.ejemplo.SpringBoot.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Ezequiel
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -55,5 +57,9 @@ public class PersonaController {
     ipersonaService.savepersona(persona);
     return persona;
     }
-   
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)5);
+    }
 }
--- Front-End/src/app/componentes/acerca-de/acerca-de.component.ts
+++ Front-End/src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'app/model/persona.model';
+import { PersonaService } from 'app/servicio/persona.service';
 
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
-
+export class AcercaDeComponent implements OnInit {
+  persona: persona = new persona("","","");
+  constructor(public personaService: PersonaService){};
+  ngOnInit(): void {
+    this.personaService.getpersona().subscribe(data => {this.persona = data});
+  }
 }
